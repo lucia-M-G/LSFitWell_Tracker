c978dbd1e9590a464a2ea90613fad9103c2d2a52
Jordi Fernández Arlegui <jordi.fernandez@gracia.lasalle.cat>
2025-04-22 08:18:32 +0200

    pas01

--- pas01.sql
+++ pas01.sql
@@ -1,32 +1,26 @@
--- Jordi Fernández Arlegui, Lucía Martínez Gutiérrez, Joan Navío García
+CREATE DATABASE IF NOT EXISTS LSFitWell;
 
--- 1. Crear la base de datos si no existe
-CREATE DATABASE IF NOT EXISTS lsfitwell;
-
--- 2. Seleccionar la base de datos
-USE lsfitwell;
-
--- 3. Creació de la taula temporal activitats_raw
-DROP TABLE IF EXISTS activitats_raw;
-
-CREATE TABLE activitats_raw (
-    id_usuari INT,
-    data_activitat DATE,
-    hora_inici TIME,
-    durada_minuts INT,
-    tipus_activitat VARCHAR(50),
-    calories INT,
-    dispositiu VARCHAR(50)
+CREATE TABLE IF NOT EXISTS activitats_raw (
+  id_usuari INT,
+  data_activitat DATE,
+  hora_inici TIME,
+  durada_minuts INT,
+  tipus_activitat VARCHAR(50),
+  calories INT,
+  dispositiu VARCHAR(20)
 );
 
--- 4. Càrrega de dades des del fitxer CSV
-LOAD DATA LOCAL INFILE 'C:\Users\Administrador\Desktop\LaSalle\WebDigitalització\Practica CV\LSFitWell_Tracker\activitats.csv'
-INTO TABLE activitats_raw
-FIELDS TERMINATED BY ',' 
-ENCLOSED BY '"'
-LINES TERMINATED BY '\n'
--- Ignora la capçalera del CSV
-IGNORE 3 ROWS;
+TRUNCATE TABLE activitats_raw;
 
--- 5. Verificació de la càrrega
-SELECT COUNT(*) AS total_registres FROM activitats_raw;
+LOAD DATA INFILE 'C:/Users/jordi/Documents/GitHub/LSFitWell_Tracker/activitats.csv'
+	INTO TABLE activitats_raw
+	FIELDS TERMINATED BY ','
+	ENCLOSED BY '"'
+	LINES TERMINATED BY '\n'
+	IGNORE 2 ROWS;
+
+SELECT COUNT(*)
+	FROM activitats_raw;
+
+SELECT *
+	FROM activitats_raw;
