a1c1a7b28806a06265d44b555b6ad710c75addb5
Jordi Fernández Arlegui <jordi.fernandez@gracia.lasalle.cat>
2025-04-22 08:13:24 +0200

    insert csv

--- script.sql
+++ script.sql
@@ -1,6 +1,6 @@
 CREATE DATABASE IF NOT EXISTS LSFitWell;
 
-CREATE TABLE activitats_raw (
+CREATE TABLE IF NOT EXISTS activitats_raw (
   id_usuari INT,
   data_activitat DATE,
   hora_inici TIME,
@@ -9,3 +9,18 @@ CREATE TABLE activitats_raw (
   calories INT,
   dispositiu VARCHAR(20)
 );
+
+TRUNCATE TABLE activitats_raw;
+
+LOAD DATA INFILE 'C:/Users/jordi/Documents/GitHub/LSFitWell_Tracker/activitats.csv'
+	INTO TABLE activitats_raw
+	FIELDS TERMINATED BY ','
+	ENCLOSED BY '"'
+	LINES TERMINATED BY '\n'
+	IGNORE 2 ROWS;
+
+SELECT COUNT(*)
+	FROM activitats_raw;
+
+SELECT *
+	FROM activitats_raw;
