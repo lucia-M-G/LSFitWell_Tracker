d55c3efe6bbb05be5f21b4086b0f996215be09c9
Jordi Fernández Arlegui <jordi.fernandez@gracia.lasalle.cat>
2025-04-22 20:47:24 +0200

    encara no funciona

---
+++ pas02.sql
@@ -0,0 +1,31 @@
+TRUNCATE TABLE activitats_net;
+
+CREATE TABLE activitats_net (
+    id_usuari INT,
+    data_activitat DATE,
+    hora_inici TIME,
+    durada_minuts INT,
+    tipus_activitat VARCHAR(50),
+    calories INT,
+	dispositiu VARCHAR(20)
+);
+
+ALTER TABLE activitats_net
+	ADD COLUMN es_cap_setmana BOOLEAN;
+
+DROP PROCEDURE netejar_dades;
+
+DELIMITER //
+CREATE PROCEDURE netejar_dades()
+BEGIN
+	INSERT INTO activitats_net
+	SELECT id_usuari, data_activitat, hora_inici, durada_minuts, 
+		tipus_activitat, calories, dispositiu,
+		DAYOFWEEK(data_activitat) IN (1,7) AS es_cap_setmana
+	FROM activitats_raw
+	WHERE data_activitat = DATE_SUB(CURDATE(), INTERVAL 1 DAY);
+END //
+DELIMITER ;
+
+SELECT *
+	FROM activitats_net;
