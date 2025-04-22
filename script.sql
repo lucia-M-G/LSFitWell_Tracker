9d67995f2426e8fc16b9ad52af31d195772381ad
Jordi Fernández Arlegui <jordi.fernandez@gracia.lasalle.cat>
2025-04-22 07:47:45 +0200

    estructura database

--- script.sql
+++ script.sql
@@ -1,1 +1,11 @@
 CREATE DATABASE IF NOT EXISTS LSFitWell;
+
+CREATE TABLE activitats_raw (
+  id_usuari INT,
+  data_activitat DATE,
+  hora_inici TIME,
+  durada_minuts INT,
+  tipus_activitat VARCHAR(50),
+  calories INT,
+  dispositiu VARCHAR(20)
+);
