373eb6c7e49e51b239d110e408109e58d0abbcba
Jordi Fernández Arlegui <jordi.fernandez@gracia.lasalle.cat>
2025-04-24 11:48:23 +0200

    pas03 acabat

--- pas02.sql
+++ pas02.sql
@@ -23,11 +23,11 @@ BEGIN
 	TRUNCATE TABLE activitats_net;
 	
     INSERT INTO activitats_net
-        SELECT id_usuari, data_activitat, hora_inici, durada_minuts, 
-            tipus_activitat, calories, dispositiu,
-            DAYOFWEEK(data_activitat) IN (1,7) AS es_cap_setmana
-        FROM activitats_raw
-        WHERE data_activitat = DATE_SUB(CURDATE(), INTERVAL 1 DAY);
+		SELECT id_usuari, data_activitat, hora_inici, durada_minuts, 
+			tipus_activitat, calories, dispositiu,
+			DAYOFWEEK(data_activitat) IN (1,7) AS es_cap_setmana
+		FROM activitats_raw
+		WHERE data_activitat = DATE_SUB(CURDATE(), INTERVAL 1 DAY);
 END //
 DELIMITER ;
 
@@ -38,7 +38,7 @@ SET GLOBAL event_scheduler=ON;
 DROP EVENT evento_netejar_dades;
 
 DELIMITER //
-	CREATE EVENT evento_netejar_original
+	CREATE EVENT evento_netejar_dades
 	ON SCHEDULE
 		EVERY 1 DAY
 		STARTS NOW()
--- pas03.sql
+++ pas03.sql
@@ -13,6 +13,8 @@ DROP PROCEDURE IF EXISTS netejar_dades;
 DELIMITER //
 CREATE PROCEDURE netejar_dades()
 BEGIN
+    DECLARE num_registres INT;
+
 	TRUNCATE TABLE activitats_net;
 	
     INSERT INTO activitats_net
@@ -22,14 +24,34 @@ BEGIN
 		FROM activitats_raw
 		WHERE data_activitat = DATE_SUB(CURDATE(), INTERVAL 1 DAY);
     
+	
+    SELECT COUNT(*) INTO num_registres FROM activitats_net;
+    
     INSERT INTO control_carregues
-		VALUES ('C:/Users/jordi/Documents/GitHub/LSFitWell_Tracker/activitats.csv', 100, NOW());
+		VALUES ('C:/Users/jordi/Documents/GitHub/LSFitWell_Tracker/activitats.csv', 
+			num_registres, NOW());
 		
-    
-    
 END //
 DELIMITER ;
 
+
+-- Event del pas02 sense modificar
+
+SET GLOBAL event_scheduler=ON;
+
+DROP EVENT evento_netejar_dades;
+
+DELIMITER //
+	CREATE EVENT evento_netejar_dades
+	ON SCHEDULE
+		EVERY 1 DAY
+		STARTS NOW()
+	DO
+	BEGIN
+		TRUNCATE TABLE activitats_net;
+		CALL netejar_dades();
+	END //
+DELIMITER ;
 CALL netejar_dades();
 
 SELECT *
