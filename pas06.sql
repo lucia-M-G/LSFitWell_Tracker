1f3ce317e95c1a25736bf9d7e7160a02e22fd16f
Jordi Fernández Arlegui <jordi.fernandez@gracia.lasalle.cat>
2025-04-25 15:53:38 +0200

    optimitzacio de memoria pas06
    triggers d'update i delete no acabats

--- pas06.sql
+++ pas06.sql
@@ -1,11 +1,12 @@
--- 1. Crear taula d'auditoria sense IDs artificials
+-- No creem un segon fitxer CSV doncs no hi ha necessitat, doncs ja podem comrpovar el seu
+-- correcte funcionament amb el CSV original
 
-DROP TABLE registre_canvis;
+-- 1. Creem tres taules per optimitzar l'espaiutilitzat, una per INSERT de activitats_net
+-- una altre pels UPDATE de md_activitat i una última pels DELETE d'aquesta última taula
 
-CREATE TABLE IF NOT EXISTS registre_canvis (
-    accio VARCHAR(10) NOT NULL, -- 'INSERT', 'UPDATE', 'DELETE'
-    taula_origen VARCHAR(20) NOT NULL, -- 'activitats_net' o 'MD_activitat'
-    
+DROP TABLE IF EXISTS registre_INSERT_activitats_net;
+
+CREATE TABLE IF NOT EXISTS registre_INSERT_activitats_net (
     -- Dades completes de l'activitat
     id_usuari INT,
     data_activitat DATE,
@@ -26,6 +27,46 @@ CREATE TABLE IF NOT EXISTS registre_canvis (
     id_registre INT AUTO_INCREMENT PRIMARY KEY
 );
 
+DROP TABLE IF EXISTS registre_DELETE_md_activitat;
+
+CREATE TABLE IF NOT EXISTS registre_DELETE_md_activitat (
+    -- Dades completes de l'activitat
+	id INT,
+    nom VARCHAR(50),
+    descripcio TEXT,
+    
+    -- Metadades
+    usuari_mysql VARCHAR(100),
+    data_hora TIMESTAMP,
+    
+    -- Detalls addicionals
+    canvis_realitzats TEXT,
+    
+	id_registre INT AUTO_INCREMENT PRIMARY KEY
+);
+
+DROP TABLE IF EXISTS registre_UPDATE_md_activitat;
+
+CREATE TABLE IF NOT EXISTS registre_UPDATE_md_activitat (
+    -- Dades completes de l'activitat
+	NEW_id INT,
+    NEW_nom VARCHAR(50),
+    NEW_descripcio TEXT,
+    
+    OLD_id INT,
+    OLD_nom VARCHAR(50),
+    OLD_descripcio TEXT,
+    
+    -- Metadades
+    usuari_mysql VARCHAR(100),
+    data_hora TIMESTAMP,
+    
+    -- Detalls addicionals
+    canvis_realitzats TEXT,
+    
+   	id_registre INT AUTO_INCREMENT PRIMARY KEY
+);
+
 -- 2. Trigger per insercions
 
 DROP TRIGGER IF EXISTS registre_canvis_insert;
@@ -35,19 +76,72 @@ CREATE TRIGGER registre_canvis_insert
 	AFTER INSERT ON activitats_net
 	FOR EACH ROW
 	BEGIN
-		INSERT INTO registre_canvis (
-			accio, taula_origen, id_usuari, data_activitat, hora_inici,
+		INSERT INTO registre_INSERT_activitats_net (
+			id_usuari, data_activitat, hora_inici,
 			durada_minuts, tipus_activitat, calories, dispositiu, es_cap_setmana,
 			usuari_mysql, data_hora, canvis_realitzats
 		)
 		VALUES (
-			'INSERT', 'activitats_net', NEW.id_usuari, NEW.data_activitat, NEW.hora_inici,
+			NEW.id_usuari, NEW.data_activitat, NEW.hora_inici,
 			NEW.durada_minuts, NEW.tipus_activitat, NEW.calories, NEW.dispositiu, NEW.es_cap_setmana,
 			CURRENT_USER(), NOW(), 'Nova activitat registrada'
 		);
 	END //
 DELIMITER ;
 
+
+-- 3. Tigger per modificacions
+
+DROP TRIGGER IF EXISTS md_activitat_update;
+
+DELIMITER //
+CREATE TRIGGER md_activitat_update
+	BEFORE UPDATE ON MD_activitat
+	FOR EACH ROW
+	BEGIN
+		DECLARE cambios TEXT;
+		
+		-- Detectar cambios específicos
+		SET cambios = '';
+		IF OLD.nom != NEW.nom THEN
+			SET cambios = CONCAT(cambios, 'Antic nom: "', OLD.nom, '"Nou nom: "', NEW.nom, '"\n'); -- Miramos la FK
+		END IF;
+		
+		IF OLD.descripcio != NEW.descripcio THEN
+			SET cambios = CONCAT(cambios, 'Descripció modificada\n'); -- Miramos la descipcion
+		END IF;
+		
+		-- Registrar solo si hay cambios reales
+		IF cambios != '' THEN
+			INSERT INTO registre_UPDATE_md_activitat (
+				
+			)
+			VALUES (
+				
+			);
+		END IF;
+	END //
+DELIMITER ;
+
+
+
+-- 4. Triger d'eliminacion
+
+DROP TRIGGER IF EXISTS md_activitat_delete;
+
+DELIMITER //
+CREATE TRIGGER md_activitat_delete
+	BEFORE DELETE ON MD_activitat
+	FOR EACH ROW
+	BEGIN
+		INSERT INTO registre_DELETE_md_activitat (
+			
+		)
+		VALUES (
+			
+		);
+	END //
+DELIMITER ;
 
 
 SELECT *
